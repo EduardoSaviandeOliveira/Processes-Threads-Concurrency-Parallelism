d8dc89bc785c736b84fa4394e221a39ac0338fd0
marcs-feh <marcosfehlauer@protonmail.com>
2023-04-08 15:17:15 -0300

    add type header

--- main.cpp
+++ main.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
+#include <array>
+#include <pthread.h>
+#include "types.h"
+
+constexpr usize BUF_SIZE = 500;
+std::array<f64, BUF_SIZE> weigh_in_buffer;
 
 int main(){
 	return 0;
